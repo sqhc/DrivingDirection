e898d2959f238f0149e507e891df3d0968fe0cec
shenqing <shenqin1@msu.edu>
2023-05-15 13:06:51 -0400

    Initial Commit

---
+++ DrivingDirection/DrivingDirectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrivingDirectionApp.swift
+//  DrivingDirection
+//
+//  Created by 沈清昊 on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DrivingDirectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
